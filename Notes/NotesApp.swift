c7fcb7ef2cea95fa51da9eb88a0798db050307fe
Jonathan_Meixueiro <lav.jonathanom@gmail.com>
2025-02-18 09:17:09 -0800

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by CIET1 on 18/02/25.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
