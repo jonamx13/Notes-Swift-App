88c8c4d34936a431e94ae2401b08cd27371c7606
Jonathan_Meixueiro <lav.jonathanom@gmail.com>
2025-02-18 10:54:43 -0800

    Image render

--- Notes/ContentView.swift
+++ Notes/ContentView.swift
@@ -9,41 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-//        Text("Don't stop believing")
-//            .font(.system(size: 50))
-//            .lineLimit(2)
-//            .multilineTextAlignment(.trailing)
-//            .bold()
-//            .underline().bold()]
-        
-//        Circle()
-//            .background(Color.green)
-//            .padding()
-//            .background(Color.red)
-//            .frame(width: 200, height: 200)
-        
-//            Rectangle()
-//            .frame(width: 200, height: 200)
-//            .foregroundStyle(Color.yellow)
-//            .cornerRadius(100)
-//            .opacity(0.5)
-        
-//        Ellipse()
-//            .stroke(lineWidth: 5)
-//            .frame(width: 200, height: 400)
-//            .foregroundStyle(Color.red)
-//            .rotationEffect(.degrees(45))
-        
-        /*Challenge: Create button with text and frame*/
+        Image("logo")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 200, height: 200)
+            .foregroundColor(Color.yellow)
+            .background(Color.gray)
         Button(action: {}) {
-            Text("Press me")
+            Image("logo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 50, height: 50)
+                .padding()
+            Text("Press wolf button")
                 .font(.headline)
-                .foregroundColor(.white)
+                .foregroundStyle(Color.white)
+                .multilineTextAlignment(.leading)
                 .padding()
                 .frame(maxWidth: .infinity)
         }
-        .background(Color.blue)
-        .cornerRadius(30)
+        .background(Color.black)
+        .cornerRadius(100)
         .padding(.horizontal)
     }
 }
