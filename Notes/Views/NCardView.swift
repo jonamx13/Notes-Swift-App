96a9bc80af8be8f086f58f857bc07ef9aa7bca5b
Jonathan_Meixueiro <lav.jonathanom@gmail.com>
2025-02-18 13:00:56 -0800

    Structuring first app

---
+++ Notes/Views/NCardView.swift
@@ -0,0 +1,18 @@
+//
+//  NCardView.swift
+//  Notes
+//
+//  Created by CIET1 on 18/02/25.
+//
+
+import SwiftUI
+
+struct NCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NCardView()
+}
