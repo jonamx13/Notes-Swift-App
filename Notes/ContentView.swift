a8d2a5e9d47c6301f91e4220708ebdd7ad4daed1
Jonathan_Meixueiro <lav.jonathanom@gmail.com>
2025-02-18 10:00:46 -0800

    Views: modifiers and shapes usage

--- Notes/ContentView.swift
+++ Notes/ContentView.swift
@@ -9,13 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+//        Text("Don't stop believing")
+//            .font(.system(size: 50))
+//            .lineLimit(2)
+//            .multilineTextAlignment(.trailing)
+//            .bold()
+//            .underline().bold()]
+        
+//        Circle()
+//            .background(Color.green)
+//            .padding()
+//            .background(Color.red)
+//            .frame(width: 200, height: 200)
+        
+//            Rectangle()
+//            .frame(width: 200, height: 200)
+//            .foregroundStyle(Color.yellow)
+//            .cornerRadius(100)
+//            .opacity(0.5)
+        
+//        Ellipse()
+//            .stroke(lineWidth: 5)
+//            .frame(width: 200, height: 400)
+//            .foregroundStyle(Color.red)
+//            .rotationEffect(.degrees(45))
+        
+        /*Challenge: Create button with text and frame*/
+        Button(action: {}) {
+            Text("Press me")
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding()
+                .frame(maxWidth: .infinity)
         }
-        .padding()
+        .background(Color.blue)
+        .cornerRadius(30)
+        .padding(.horizontal)
     }
 }
 
