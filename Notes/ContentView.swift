9c16896002816d5f74a0cc66d2ec418eb9db77b0
Jonathan_Meixueiro <lav.jonathanom@gmail.com>
2025-02-18 11:50:28 -0800

    Layouts with Stacks (HStack, VStack, ZStack)

--- Notes/ContentView.swift
+++ Notes/ContentView.swift
@@ -9,28 +9,89 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Image("logo")
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-            .frame(width: 200, height: 200)
-            .foregroundColor(Color.yellow)
-            .background(Color.gray)
-        Button(action: {}) {
-            Image("logo")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 50, height: 50)
-                .padding()
-            Text("Press wolf button")
+//        VStack(alignment: .center, spacing: 20) {
+//            Text("Don't")
+//            Text("stop")
+//            Text("believing")
+//            Text("Don't stop believing")
+//        }
+//        HStack(alignment: .bottom, spacing: 20) {
+//            Text("Don't")
+//            Text("stop")
+//                .font(.largeTitle)
+//            Text("believing")
+//            Text("Don't stop believing")
+//        }
+//        ZStack(alignment: .topLeading) {
+//            Color.green
+//            Circle()
+//            Rectangle()
+//                .foregroundStyle(Color.yellow)
+//                .frame(width: 100, height: 100)
+//            Text("Hello, World!")
+//        }
+//        ZStack(alignment: .center) {
+//            Color.green
+//            Circle()
+//            HStack(spacing: 0) {
+//                Rectangle()
+//                    .foregroundStyle(Color.yellow)
+//                    .frame(width: 100, height: 100)
+//                Rectangle()
+//                    .foregroundStyle(Color.red)
+//                    .frame(width: 100, height: 100)
+//            }
+//            VStack {
+//                Text("Hello")
+//                Text("World!")
+//            }
+//        }
+        
+        // Little Card
+        HStack {
+            Text("Hello, world!")
                 .font(.headline)
-                .foregroundStyle(Color.white)
+                .lineLimit(2)
+                .frame(width: 100)
+                .padding(8)
+                .background(Color.cyan.opacity(0.2))
+                .cornerRadius(8)
+            Text("This is an example of a preview.")
+                .font(.subheadline)
+                .lineLimit(2)
                 .multilineTextAlignment(.leading)
-                .padding()
+                .frame(width: .infinity)
+            Image(systemName: "heart")
+                .foregroundStyle(Color.red)
+        }
+        .padding()
+        .background(Color.gray.opacity(0.1))
+        .cornerRadius(16)
+        
+        // Big Card
+        VStack {
+            HStack {
+                Spacer()
+                Image(systemName: "heart")
+                    .foregroundStyle(Color.red)
+            }
+            Text("Hello World")
+                .font(.title2)
+                .lineLimit(2)
+                .padding(.vertical, 8)
+                .padding(.horizontal, 30)
+                .background(Color.cyan.opacity(0.2))
+                .cornerRadius(8)
+            Text("This is an example of a preview.")
+                .font(.body)
+                .lineLimit(2)
+                .multilineTextAlignment(.leading)
                 .frame(maxWidth: .infinity)
         }
-        .background(Color.black)
-        .cornerRadius(100)
-        .padding(.horizontal)
+        .frame(height: 150)
+        .padding()
+        .background(Color.gray.opacity(0.1))
+        .cornerRadius(16)
     }
 }
 
