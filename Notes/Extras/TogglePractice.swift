c44b71a62bbeceb7c75608e8234019d487629d13
Jonathan_Meixueiro <lav.jonathanom@gmail.com>
2025-02-26 09:27:52 -0800

    Toggle and Slider

---
+++ Notes/Extras/TogglePractice.swift
@@ -0,0 +1,18 @@
+//
+//  TogglePractice.swift
+//  Notes
+//
+//  Created by CIET1 on 26/02/25.
+//
+
+import SwiftUI
+
+struct TogglePractice: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TogglePractice()
+}
