681141e5618f280674444e22f35694a7a3bf694c
Jonathan_Meixueiro <lav.jonathanom@gmail.com>
2025-02-26 10:46:52 -0800

    Alerts and Modals

---
+++ Notes/Extras/AlertPractice.swift
@@ -0,0 +1,18 @@
+//
+//  AlertPractice.swift
+//  Notes
+//
+//  Created by CIET1 on 26/02/25.
+//
+
+import SwiftUI
+
+struct AlertPractice: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AlertPractice()
+}
