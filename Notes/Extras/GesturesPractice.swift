1c451bfee4ab89850b0d6fe6082771b9dec5698e
Jonathan_Meixueiro <lav.jonathanom@gmail.com>
2025-02-27 08:27:38 -0800

    Gestures

---
+++ Notes/Extras/GesturesPractice.swift
@@ -0,0 +1,18 @@
+//
+//  GesturesPractice.swift
+//  Notes
+//
+//  Created by CIET1 on 27/02/25.
+//
+
+import SwiftUI
+
+struct GesturesPractice: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GesturesPractice()
+}
